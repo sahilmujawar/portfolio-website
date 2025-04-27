bae91b5159383a624f1dbc5eaa0a75b43fcbe6bd
Sahil Mujawar <sahilshael70@yahoo.com>
2025-04-27 12:41:42 -0400

    Portfolio-website

---
+++ script.js
@@ -0,0 +1,17 @@
+function toggleMenu() {
+  const menu = document.querySelector(".menu-links");
+  const icon = document.querySelector(".hamburger-icon");
+  menu.classList.toggle("open");
+  icon.classList.toggle("open");
+}
+
+document.getElementById("year").textContent = new Date().getFullYear();
+
+function openModal(id) {
+  document.getElementById(id).style.display = "block";
+}
+
+function closeModal(id) {
+  document.getElementById(id).style.display = "none";
+}
+
